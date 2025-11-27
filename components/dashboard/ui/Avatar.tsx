865d4bfecbe42ddeb9f7686d0132ed233a3ada7f
Chinemerem Ugbaja <marycynthiaconfirm@gmail.com>
2025-11-27 16:22:01 +0000

    change the forgot password page extension from ts to tsx

--- components/dashboard/ui/Avatar.tsx
+++ components/dashboard/ui/Avatar.tsx
@@ -2,7 +2,7 @@ const Avatar = ({isAvatar}:{isAvatar: boolean}) => {
   return (
     <div className={`  ${!isAvatar? "w-8 h-8" : "w-10 h-10 md:w-18 md:h-18"}`}>
       <img
-        src="/chinemerem.jpeg"
+        src="/default-avartar.jpg"
         alt="user avatar"
         className="h-full w-full rounded-full max-w-full "
       />
